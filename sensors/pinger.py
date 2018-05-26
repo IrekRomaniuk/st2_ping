37d0c70258547f6915e4be323655e2258b24a4ab
irom77 <aniairom0>
2018-05-26 13:49:23 -0400

    testing

--- sensors/pinger.py
+++ sensors/pinger.py
@@ -22,8 +22,9 @@ class Pinger(PollingSensor):
                                           config=config,                                 
                                           poll_interval=poll_interval)
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
+        self.ips_q = Queue.Queue()
+        self.out_q = Queue.Queue()
        
-
         
     def setup(self):               
         self._timeout=int(self._config['timeout'])
@@ -35,8 +36,7 @@ class Pinger(PollingSensor):
         self._logger.debug('########## Pinger dispatching trigger...')   
         # 
         num_threads = self._threads
-        ips_q = Queue.Queue()
-        out_q = Queue.Queue()
+        
         payload={}
         ips = []
         """
@@ -49,21 +49,21 @@ class Pinger(PollingSensor):
         self._logger.debug('########## First: {} Last: {} Number: {}'.format(ips[0],ips[len(ips)-1], len(ips)))
         # start the thread pool
         for i in range(num_threads):
-            worker = Thread(target=self.thread_pinger, args=(i, ips_q))
+            worker = Thread(target=self.thread_pinger, args=(i, self.ips_q))
             worker.setDaemon(True)
             worker.start()
 
         # fill queue
         for ip in ips:
-            ips_q.put(ip)
+            self.ips_q.put(ip)
 
         # wait until worker threads are done to exit
-        ips_q.join()
+        self.ips_q.join()
 
         # print result
         while True:
             try:
-                msg = out_q.get_nowait()
+                msg = self.out_q.get_nowait()
             except Queue.Empty:
                 break
             self._logger.debug('##########  Result: {}'.format(msg))
@@ -87,7 +87,7 @@ class Pinger(PollingSensor):
                 # rtt min/avg/max/mdev = 22.293/22.293/22.293/0.000 ms
                 search = re.search(r'rtt min/avg/max/mdev = (.*)/(.*)/(.*)/(.*) ms', p_ping_out, re.M|re.I)
                 ping_rtt = search.group(2)
-                out_q.put("OK " + str(ip) + " rtt= "+ ping_rtt)
+                self.out_q.put("OK " + str(ip) + " rtt= "+ ping_rtt)
 
             # update queue : this ip is processed
             q.task_done()
