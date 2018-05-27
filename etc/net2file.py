41872d0d277fc5eb0485a44c210fb06aa6a3a4ca
irom77 <aniairom0>
2018-05-27 10:30:12 -0400

    testing

--- etc/net2file.py
+++ etc/net2file.py
@@ -1,7 +1,7 @@
 import ipaddress
 
 ips = []
-my_net=ipaddress.ip_network(u'10.29.0.0/20') #10.220.192.192/29
+my_net=ipaddress.ip_network(u'10.29.0.0/21') #10.220.192.192/29
 for host in my_net.hosts():
   #if (str(host).split('.')[3]) == '1':
     #print(host)
